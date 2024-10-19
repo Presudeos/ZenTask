294af70ef6ae0aeaf267d1756d3b7a1a20a723a2
Virya <162869618+acvirya@users.noreply.github.com>
2024-10-20 06:20:57 +0700

    Added UI for Add, Edit & Delete Task, animation bug fix

--- UTS/page/task.php
+++ UTS/page/task.php
@@ -31,7 +31,7 @@
     <!-- JQuery -->
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 </head>
-<body>
+<body style="background-color:#fafafa">
     <!-- Navigation -->
     <nav class="w-full h-auto py-5 flex items-center px-[20px] md:px-[55px] max-lg:justify-between">
         <div class="brand w-auto font-[Pacifico] text-3xl md:text-4xl basis-1/4">
@@ -61,7 +61,7 @@
         </div>
     </nav>
 
-
+    <div class="bgblur">
     <div class="w-4/5 mx-auto md:w-[600px]">
         <!-- Searchbar -->
         <div class="searchbar w-full h-12 mt-3 mb-5 flex wrap">
@@ -74,53 +74,21 @@
         ></input>
         </div>
 
-        <!-- Add task -->
+        <!-- Add task button -->
          <div class="flex w-full h-auto justify-center items-center md:justify-between flex-col md:flex-row gap-y-3">
             <p class="font-[Poppins] font-bold text-xl">All your tasks:</p>
-            <button class="button1" type="button">
-                <span class="button1__text font-[Roboto] font-bold text-base">Add Task</span>
-                <span class="button1__icon"><svg class="svg" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><line x1="12" x2="12" y1="5" y2="19"></line><line x1="5" x2="19" y1="12" y2="12"></line></svg></span>
+            <button class="button1 addtask" type="button">
+                <span class="button1__text font-[Roboto] font-bold text-base addtask">Add Task</span>
+                <span class="button1__icon addtask"><svg class="svg addtask" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><line class="addtask" x1="12" x2="12" y1="5" y2="19"></line><line class="addtask" x1="5" x2="19" y1="12" y2="12"></line></svg></span>
             </button>
          </div>
         <!-- Task List -->
         <ul class="text-base mt-3 md:mt-2">
-            <li class="list-container flex items-center py-3 font-[Montserrat] flex-wrap hover:bg-blue-100 rounded-md px-4 cursor-pointer">
-                <div class="content-container flex flex-row w-full items-center justify-between">
-                    <input type="checkbox" id="list1" class="cbx" style="display: none;">
-                    <label for="list1" class="check m-0">
-                        <svg width="18px" height="18px" viewBox="0 0 18 18">
-                            <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z"></path>
-                            <polyline points="1 9 7 14 15 4"></polyline>
-                        </svg>
-                    </label>
-                    <p class="title ml-4 cursor-pointer">Task 1</p>
-                    <p class="deadline mr-4 cursor-pointer max-sm:place-self-end ml-auto">Yesterday, 15:00</p>
-                </div>
-                <div class="description-div ml-9 cursor-default">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, inventore?</p>
-                </div>
-            </li>
-            <li class="list-container flex items-center py-3 font-[Montserrat] flex-wrap hover:bg-blue-100 rounded-md px-4">
-                <div class="content-container flex flex-row w-full items-center justify-between">
-                    <input type="checkbox" id="list2" class="cbx" style="display: none;">
-                    <label for="list2" class="check m-0">
-                        <svg width="18px" height="18px" viewBox="0 0 18 18">
-                            <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z"></path>
-                            <polyline points="1 9 7 14 15 4"></polyline>
-                        </svg>
-                    </label>
-                    <p class="title ml-4 cursor-pointer">Task 2</p>
-                    <p class="deadline mr-4 cursor-pointer max-sm:place-self-end ml-auto">Today, 09:00</p>
-                </div>
-                <div class="description-div ml-9 cursor-default">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, inventore?</p>
-                </div>
-            </li>
             <li class="list-container flex items-center py-3 font-[Montserrat] flex-wrap hover:bg-blue-100 rounded-md px-4">
                 <div class="content-container flex flex-row w-full items-center justify-between h-full">
                     <input type="checkbox" id="list3" class="cbx" style="display: none;">
-                    <label for="list3" class="check m-0">
-                        <svg width="18px" height="18px" viewBox="0 0 18 18">
+                    <label for="list3" class="check m-0 cb">
+                        <svg width="18px" height="18px" viewBox="0 0 18 18" class="cb">
                             <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z"></path>
                             <polyline points="1 9 7 14 15 4"></polyline>
                         </svg>
@@ -130,52 +98,189 @@
                 </div>
                 <div class="description-div ml-9 cursor-default">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, inventore?</p>
+                    <div class="flex flex-row justify-end edit-container">
+                        <button class="w-auto h-auto px-3 py-1 rounded-md edit-box add"><img class="w-5 edit-box" src="./images/edit.svg"></img></button>
+                        <button class="w-auto h-auto px-3 py-1 rounded-md delete-box delete"><img class="w-5 delete-box" src="./images/delete.svg"></img></button>
+                    </div>
                 </div>
             </li>
         </ul>
-        
+    </div>
+    </div>
+
+    <!-- Modal: Add Task -->
+    <div class="modal-container-addtask fixed top-0 left-0 h-full w-full flex items-center justify-center hidden">
+        <form action="#" method="POST" class="modal-addtask w-96 px-8 py-8 flex flex-col justify-center bg-white rounded-md shadow font-[Roboto]">
+            <p class="font-bold place-self-center">Add Task</p>
+            <div class="flex flex-col mb-3 mt-4 text-sm gap-y-1">
+                <label>Title</label>
+                <input class="h-10 p-3 border rounded-md" name="title" placeholder="Input task title"/>
+            </div>
+            <div class="flex flex-col mb-3 text-sm gap-y-1">
+                <label>Description</label>
+                <textarea class="h-20 p-3 border rounded-md" name="description" placeholder="Input task description"></textarea>
+            </div>
+            <div class="flex flex-col mb-3 text-sm gap-y-1">
+                <label>Deadline</label>
+                <div class="flex flex-row justify-between gap-x-4">
+                    <input type="date" class="w-40 h-10 p-3 border rounded-md" name="day" placeholder="Date"/>
+                    <input type="time" class="w-40 h-10 p-3 border rounded-md" name="month" placeholder="Time" value="00:00"/>
+                </div>
+            </div>
+
+            <div class="flex flex-row mt-4  justify-between">
+                <button class="cancelform-addtask w-28 h-8 rounded-md bg-gray-400 text-md text-white">Cancel</button>
+                <input type="submit" class="w-28 h-8 rounded-md bg-green-500 text-md text-white flex justify-center items-center cursor-pointer">
+            </div>
+        </form>
+    </div>
+
+    <!-- Modal: Edit Task -->
+    <div class="modal-container-edittask fixed top-0 left-0 h-full w-full flex items-center justify-center hidden">
+        <form action="#" method="POST" class="modal-edittask w-96 px-8 py-8 flex flex-col justify-center bg-white rounded-md shadow font-[Roboto]">
+            <p class="font-bold place-self-center">Edit Task</p>
+            <div class="flex flex-col mb-3 mt-4 text-sm gap-y-1">
+                <label>Title</label>
+                <input class="h-10 p-3 border rounded-md" name="title" placeholder="Input task title"/>
+            </div>
+            <div class="flex flex-col mb-3 text-sm gap-y-1">
+                <label>Description</label>
+                <textarea class="h-20 p-3 border rounded-md" name="description" placeholder="Input task description"></textarea>
+            </div>
+            <div class="flex flex-col mb-3 text-sm gap-y-1">
+                <label>Deadline</label>
+                <div class="flex flex-row justify-between gap-x-4">
+                    <input type="date" class="w-40 h-10 p-3 border rounded-md" name="day" placeholder="Date"/>
+                    <input type="time" class="w-40 h-10 p-3 border rounded-md" name="month" placeholder="Time" value="00:00"/>
+                </div>
+            </div>
+
+            <div class="flex flex-row mt-4  justify-between">
+                <button type="button" class="cancelform-edittask w-28 h-8 rounded-md bg-gray-400 text-md text-white">Cancel</button>
+                <input type="submit" class="w-28 h-8 rounded-md bg-green-500 text-md text-white flex justify-center items-center cursor-pointer">
+            </div>
+        </form>
+    </div>
+
+    <!-- Modal: Delete Task -->
+    <div class="modal-container-deletetask fixed top-0 left-0 h-full w-full flex items-center justify-center hidden">
+        <form action="#" method="POST" class="modal-deletetask w-96 px-8 py-8 flex flex-col justify-center bg-white rounded-md shadow font-[Roboto]">
+            <p class="font-bold place-self-center">Delete Task</p>
+
+            <p class="place-self-center pt-5 pb-3">Are you sure want to delete this task?</p>
+
+            <div class="flex flex-row mt-4  justify-between">
+                <button type="button" class="cancelform-deletetask w-28 h-8 rounded-md bg-gray-400 text-md text-white">Cancel</button>
+                <input type="submit" class="w-28 h-8 rounded-md bg-green-500 text-md text-white flex justify-center items-center cursor-pointer">
+            </div>
+        </form>
     </div>
 </body>
 
 <script>
-
 $(document).ready(function() {
+    // Meng-handle klik pada checkbox di dalam li
+    $('input[type="checkbox"]').on('click', function(e) {
+        e.stopPropagation();
+
+        var $checkbox = $(this); 
+        var $listItem = $checkbox.closest('.list-container');
+
+        $listItem.addClass("remove");
+    });
+
     // Meng-handle klik pada list item (li) atau elemen di dalamnya
-    $('.list-container').on('click', function(e) {
-        // Cegah event dari checkbox agar tidak ikut ter-trigger
-        if ($(e.target).is('input[type="checkbox"]') || $(e.target).is('label')) {
-            return;
-        }
+    $('li').on('click', function(e) {
+        if ($(e.target).hasClass('cb') || $(e.target).hasClass('edit-box') || $(e.target).hasClass('delete-box')) return;
+        console.log(e.target);
+        var $clickedList = $(this);  
+        var $descriptionDiv = $clickedList.find('.description-div'); 
 
-        var $clickedList = $(this);  // elemen li yang diklik
-        var $descriptionDiv = $clickedList.find('.description-div'); // div deskripsi dalam li
-
-        // Jika ada elemen yang sudah di-expand, hilangkan class .expand terlebih dahulu
         $('.description-div.expand').not($descriptionDiv).removeClass('expand');
-
-        // Toggle class .expand untuk description di dalam list yang diklik
         $descriptionDiv.toggleClass('expand');
     });
 
-    // Meng-handle klik pada checkbox di dalam li
-    $('input[type="checkbox"]').on('click', function() {
-        var $checkbox = $(this);  // checkbox yang diklik
-        var $listItem = $checkbox.closest('.list-container'); // list item (li) dari checkbox
-
-        // Tambahkan attribute disabled ke checkbox yang diklik
-        $checkbox.prop('disabled', true);
-        $checkbox.parent().parent().addClass("remove");
-
-
-        // Hapus list item setelah 1 detik
-        setTimeout(function() {
-            $listItem.fadeOut(400, function() {
-                $(this).remove();
-            });
-        }, 1000);
+    // Meng-handle klik pada add task button
+    $('.addtask').on('click', function(e) {
+        console.log(e.target);
+        var $clicked = $(document);
+        var $descriptionDiv = $clicked.find('.modal-container-addtask'); 
+        var $blurContainer = $clicked.find('.bgblur');
+        
+        $descriptionDiv.removeClass('hidden');
+        $blurContainer.addClass('blur');
     });
-});
 
+    // Meng-handle klik pada edit task button
+    $('.edit-box').on('click', function(e) {
+        console.log(e.target);
+        var $clicked = $(document);
+        var $descriptionDiv = $clicked.find('.modal-container-edittask'); 
+        var $blurContainer = $clicked.find('.bgblur');
+        
+        $descriptionDiv.removeClass('hidden');
+        $blurContainer.addClass('blur');
+    });
+
+    // Meng-handle klik pada delete task button
+    $('.delete-box').on('click', function(e) {
+        console.log(e.target);
+        var $clicked = $(document);
+        var $descriptionDiv = $clicked.find('.modal-container-deletetask'); 
+        var $blurContainer = $clicked.find('.bgblur');
+        
+        $descriptionDiv.removeClass('hidden');
+        $blurContainer.addClass('blur');
+    });
+
+    // Meng-handle klik pada cancel form Add Task
+    $('.cancelform-addtask').on('click', function(e) {
+        console.log(e.target);
+        e.preventDefault();
+        var $clicked = $(document);  
+        var $descriptionDiv = $clicked.find('.modal-container-addtask'); 
+        var $blurContainer = $clicked.find('.bgblur');
+
+        $clicked.find('form')[0].reset();
+
+        $descriptionDiv.addClass('hidden');
+        $blurContainer.removeClass('blur');
+    });
+
+    // Meng-handle klik pada cancel form Edit Task
+    $('.cancelform-edittask').on('click', function(e) {
+        console.log(e.target);
+        e.preventDefault();
+        var $clicked = $(document);  
+        var $descriptionDiv = $clicked.find('.modal-container-edittask'); 
+        var $blurContainer = $clicked.find('.bgblur');
+
+        console.log($clicked.find('form')[1]);
+
+        $clicked.find('form')[1].reset();
+
+        $descriptionDiv.addClass('hidden');
+        $blurContainer.removeClass('blur');
+    });
+
+    // Meng-handle klik pada cancel form Delete Task
+    $('.cancelform-deletetask').on('click', function(e) {
+        console.log(e.target);
+        e.preventDefault();
+        var $clicked = $(document);  
+        var $descriptionDiv = $clicked.find('.modal-container-deletetask'); 
+        var $blurContainer = $clicked.find('.bgblur');
+
+        console.log($clicked.find('form')[2]);
+
+        $clicked.find('form')[2].reset();
+
+        $descriptionDiv.addClass('hidden');
+        $blurContainer.removeClass('blur');
+    });
+    
+});
 </script>
+
 
 </html>
