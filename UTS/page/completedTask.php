435f854c9b45c93cef75bf0938bd63bb6727ed2c
Virya <162869618+acvirya@users.noreply.github.com>
2024-10-20 20:00:48 +0700

    RESPONSIVE

--- UTS/page/completedTask.php
+++ UTS/page/completedTask.php
@@ -1,3 +1,5 @@
+<!-- Responsive -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +15,7 @@
       rel="stylesheet"
     />
     <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap"
       rel="stylesheet"
     />
     <link
@@ -21,7 +23,7 @@
       rel="stylesheet"
     />
     <link
-      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900&display=swap"
       rel="stylesheet"
     />
 
@@ -55,8 +57,8 @@
                 <img
                     src="./images/user_profile.jpg"
                     class="cursor-pointer rounded-full object-cover h-12 ml-4"
-                    alt=""
-                ></img>
+                    alt="Profile Picture"
+                />
             </a>
             <a href="profile.php">
                 <p class="cursor-pointer font-[Montserrat] font-light text-md text-right max-[1200px]:hidden">
@@ -67,11 +69,11 @@
     </nav>
 
     <div class="bgblur">
-    <div class="w-4/5 mx-auto md:w-[600px]">
+    <div class="w-11/12 mx-auto md:w-[600px]">
         <!-- Searchbar -->
         <div class="searchbar w-full h-12 mt-3 mb-7 flex wrap">
         <div class="searchbar-icon-box h-12 w-10 flex justify-left items-center shrink-0">
-            <img class="pt-1 w-6" src="./images/searchicon.svg"></img>
+            <img class="pt-1 w-6" src="./images/searchicon.svg" alt="Search Icon"></img>
         </div>
         <input
             class="w-full h-12 bg-transparent font-[Roboto] text-base px-1 outline-none"
@@ -80,10 +82,10 @@
         </div>
 
         <!-- Add task button -->
-         <div class="flex w-full h-auto justify-center items-center md:justify-between flex-col md:flex-row gap-y-3">
+         <div class="flex w-full h-auto justify-between flex-col-reverse md:flex-row gap-y-3">
             <p class="font-[Poppins] font-bold text-xl">Completed tasks:</p>
-            <p class="delete-box-ALL text-blue-500 underline cursor-pointer">Remove All Completed Task</p>
          </div>
+
         <!-- Task List -->
         <ul class="text-base mt-3 md:mt-2">
             <li class="list-container flex items-center py-3 font-[Montserrat] flex-wrap hover:bg-blue-100 rounded-md px-4">
@@ -94,36 +96,41 @@
                 <div class="description-div ml-9 cursor-default">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, inventore?</p>
                     <div class="flex flex-row justify-end edit-container">
-                        <button class="w-auto h-auto px-3 py-1 rounded-md delete-box delete"><img class="w-5 delete-box" src="./images/delete.svg"></img></button>
+                        <button class="w-auto h-auto px-3 py-1 rounded-md delete-box delete"><img class="w-5 delete-box" src="./images/delete.svg" alt="Delete"></img></button>
                     </div>
                 </div>
             </li>
         </ul>
+
+        <!-- Remove All Completed Task -->
+        <div class="w-full flex justify-start order-last mt-4">
+            <p class="delete-box-ALL text-blue-500 underline cursor-pointer">Remove All Completed Task</p>
+        </div>
     </div>
     </div>
 
     <!-- Modal: Delete Task -->
     <div class="modal-container-deletetask fixed top-0 left-0 h-full w-full flex items-center justify-center hidden">
-        <form action="#" method="POST" class="modal-deletetask w-96 px-8 py-8 flex flex-col justify-center bg-white rounded-md shadow font-[Roboto]">
+        <form action="#" method="POST" class="modal-deletetask w-11/12 max-w-md px-8 py-8 flex flex-col justify-center bg-white rounded-md shadow font-[Roboto]">
             <p class="font-bold place-self-center">Delete Task</p>
 
-            <p class="delete-box place-self-center pt-5 pb-3">Are you sure want to delete this task?</p>
+            <p class="delete-box place-self-center pt-5 pb-3">Are you sure you want to delete this task?</p>
 
-            <div class="flex flex-row mt-4  justify-between">
+            <div class="flex flex-row mt-4 justify-between">
                 <button type="button" class="cancelform-deletetask w-28 h-8 rounded-md bg-gray-400 text-md text-white">Cancel</button>
                 <input type="submit" class="w-28 h-8 rounded-md bg-green-500 text-md text-white flex justify-center items-center cursor-pointer">
             </div>
         </form>
     </div>
 
-    <!-- Modal: Delete All Task -->
+    <!-- Modal: Delete All Tasks -->
     <div class="modal-container-deletetaskALL fixed top-0 left-0 h-full w-full flex items-center justify-center hidden">
-        <form action="#" method="POST" class="modal-deletetask w-96 px-8 py-8 flex flex-col justify-center bg-white rounded-md shadow font-[Roboto]">
-            <p class="delete-box-ALL font-bold place-self-center">Delete Task</p>
+        <form action="#" method="POST" class="modal-deletetask w-11/12 max-w-md px-8 py-8 flex flex-col justify-center bg-white rounded-md shadow font-[Roboto]">
+            <p class="delete-box-ALL font-bold place-self-center">Delete All Tasks</p>
 
-            <p class="place-self-center pt-5 pb-3">Are you sure want to delete all task?</p>
+            <p class="place-self-center pt-5 pb-3">Are you sure you want to delete all tasks?</p>
 
-            <div class="flex flex-row mt-4  justify-between">
+            <div class="flex flex-row mt-4 justify-between">
                 <button type="button" class="cancelform-deletetaskALL w-28 h-8 rounded-md bg-gray-400 text-md text-white">Cancel</button>
                 <input type="submit" class="w-28 h-8 rounded-md bg-green-500 text-md text-white flex justify-center items-center cursor-pointer">
             </div>
@@ -133,19 +140,9 @@
 
 <script>
 $(document).ready(function() {
-    // Meng-handle klik pada checkbox di dalam li
-    $('input[type="checkbox"]').on('click', function(e) {
-        e.stopPropagation();
-
-        var $checkbox = $(this); 
-        var $listItem = $checkbox.closest('.list-container');
-
-        $listItem.addClass("remove");
-    });
-
     // Meng-handle klik pada list item (li) atau elemen di dalamnya
     $('li').on('click', function(e) {
-        if ($(e.target).hasClass('cb') || $(e.target).hasClass('edit-box') || $(e.target).hasClass('delete-box')) return;
+        if ($(e.target).hasClass('cb') || $(e.target).hasClass('delete-box')) return;
         console.log(e.target);
         var $clickedList = $(this);  
         var $descriptionDiv = $clickedList.find('.description-div'); 
@@ -173,16 +170,11 @@ $(document).ready(function() {
         var $descriptionDiv = $clicked.find('.modal-container-deletetask'); 
         var $blurContainer = $clicked.find('.bgblur');
 
-        console.log($clicked.find('form')[0]);
-
-        $clicked.find('form')[0].reset();
-
         $descriptionDiv.addClass('hidden');
         $blurContainer.removeClass('blur');
     });
 
-
-    // Meng-handle klik pada delete task button
+    // Meng-handle klik pada delete all task button
     $('.delete-box-ALL').on('click', function(e) {
         console.log(e.target);
         var $clicked = $(document);
@@ -193,7 +185,7 @@ $(document).ready(function() {
         $blurContainer.addClass('blur');
     });
 
-    // Meng-handle klik pada cancel form Delete Task
+    // Meng-handle klik pada cancel form Delete All Tasks
     $('.cancelform-deletetaskALL').on('click', function(e) {
         console.log(e.target);
         e.preventDefault();
@@ -201,16 +193,9 @@ $(document).ready(function() {
         var $descriptionDiv = $clicked.find('.modal-container-deletetaskALL'); 
         var $blurContainer = $clicked.find('.bgblur');
 
-        console.log($clicked.find('form')[2]);
-
-        $clicked.find('form')[0].reset();
-
         $descriptionDiv.addClass('hidden');
         $blurContainer.removeClass('blur');
     });
-    
 });
 </script>
-
-
 </html>
